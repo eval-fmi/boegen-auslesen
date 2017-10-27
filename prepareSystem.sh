19d711593b951a3b88e04fda316845ecebee9cc9
Evaluation FMI <eval-fmi@uni-jena.de>
2017-10-27 17:05:49 +0200

    fresh start

---
+++ prepareSystem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install -y python3
+sudo apt install -y python3-pip
+sudo apt install -y python3-tk
+sudo apt install -y python3-pil.imagetk
+sudo apt install -y tesseract-ocr
+sudo apt install -y tesseract-ocr-deu
+pip3 install numpy
+pip3 install python_levenshtein

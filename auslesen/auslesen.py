1dddea754d5a4d6d94c33ef4440570f3033aba9a
= <=>
2019-02-12 17:34:50 +0100

    refactor readOutQuestionaires für Test + auslagern in auslesen.py

---
+++ auslesen/auslesen.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import logging
+
+import readOutQuestionaires
+
+logging.basicConfig(filename=f'{(__file__)[:-3]}.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s', datefmt = "%H:%M-%x")
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler())
+
+if __name__ == "__main__":
+
+    argv = sys.argv
+# TODO muss in die Extra-Datei auslesen umgelagert werden (mit raise Exc.)
+    if len(argv)<2:
+        logger.error('Fehlendes Argument.', exec = True)
+        exit()
+
+    path = argv[1]
+    if not os.path.isdir(path):
+        logger.error(path + " ist kein Verzeichnis.", exec = True)
+    if not path.endswith("/"):
+        path += "/"
+
+    fbs_liste = readOutQuestionaires.fbs_vorbereiten(path)
+    fbs_fertig_liste = readOutQuestionaires.phase1_durchfuehren
+    readOutQuestionaires.daten_speichern(fbs_fertig_liste)

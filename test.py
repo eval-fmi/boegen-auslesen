3cd3bdb0c9b4e3d7180c550d01bc6a778caa6b56
eval-fmi <eval-fmi@uni-jena.de>
2019-02-24 20:42:39 +0100

    Refactoring: Testing wird in Root-Ordner übernommen

---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+import inspect
+import os
+import logging
+from pathlib import Path
+
+logging.basicConfig(filename=f'{(__file__)[:-3]}.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s', datefmt = "%H:%M-%x")
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler())
+
+import auslesen.readOutQuestionaires as readOutQuestionaires
+import auslesen.readOutQuestionaires_old as readOutQuestionaires_old
+
+class AuslesenTest (unittest.TestCase):
+
+    def test_testordner(self):
+
+        root = Path(os.path.abspath(__file__))
+        print(root)
+        test_ordner = root.parent /'test_Boegen'
+        return test_ordner
+        
+
+    def test_frageboegen_lesen(self):
+
+        testordner = self.test_testordner()
+        testordner = str(testordner.resolve())
+        fbs_liste = readOutQuestionaires.fbs_vorbereiten(testordner)
+        print("vorbereitung abgeschlossen")
+        fbs_fertig_liste = readOutQuestionaires.phase1_durchfuehren(fbs_liste)
+        print("phase1 durchgeführt")
+        readOutQuestionaires.daten_speichern(fbs_fertig_liste)
+        print("alle daten gespeichert")
+
+if __name__ == '__main__':
+    unittest.main()
